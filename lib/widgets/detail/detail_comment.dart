13968a0732e5ee360ae142a63a1731a67f7b3f6d
박준수 <jspark9703@gmail.com>
2023-12-05 12:35:12 +0900

    detail Screen 조금 손봄

--- lib/widgets/detail/detail_comment.dart
+++ lib/widgets/detail/detail_comment.dart
@@ -24,74 +24,78 @@ class _DetailCommentState extends State<DetailComment> {
     };
 
     return Consumer<ApplicationState>(
-        builder: (context, appState, _) => appState.loggedIn
-            ? appState.emailVerified
-                ? Column(
-                    children: [
-                      TextField(
-                        controller: textEditingController,
-                        maxLines: 1,
-                        onSubmitted: (value) {
-                          setState(() {
-                            movieComment = <String, dynamic>{
-                              "movie_id": widget.movieId,
-                              "user_email": appState.userEmail,
-                              "comment": value,
-                            };
-                          });
-                          db.collection("movieComment").add(movieComment).then(
-                              (DocumentReference doc) => print(
-                                  'DocumentSnapshot added with ID: ${doc.id}'));
+        builder: (context, appState, _) => Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                appState.loggedIn
+                    ? appState.emailVerified
+                        ? TextField(
+                            controller: textEditingController,
+                            maxLines: 1,
+                            onSubmitted: (value) {
+                              setState(() {
+                                movieComment = <String, dynamic>{
+                                  "movie_id": widget.movieId,
+                                  "user_email": appState.userEmail,
+                                  "comment": value,
+                                };
+                              });
+                              db
+                                  .collection("movieComment")
+                                  .add(movieComment)
+                                  .then((DocumentReference doc) => print(
+                                      'DocumentSnapshot added with ID: ${doc.id}'));
+                            },
+                          )
+                        : const Center(child: Text("이메일 인증을하여 댓글을 달아보세요"))
+                    : const Center(child: Text("로그인하여 댓글을 달아보세요.")),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 300,
+                  child: FutureBuilder<QuerySnapshot>(
+                    future: db
+                        .collection("movieComment")
+                        .where("movie_id", isEqualTo: widget.movieId)
+                        .get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                            child:
+                                CircularProgressIndicator()); // Display a loading indicator while fetching data.
+                      }
+
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      }
+
+                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                        return const Text('No comments available.');
+                      }
+
+                      // Replace 1 with the actual number of comments you want to display.
+                      int itemCount = snapshot.data!.size;
+
+                      return ListView.builder(
+                        itemCount: itemCount,
+                        itemBuilder: (context, index) {
+                          var data = snapshot.data!.docs[index];
+                          String comment = data["comment"];
+                          String user = data["user_email"];
+                          return ListTile(
+                            contentPadding: const EdgeInsets.all(8),
+                            leading: Text(user),
+                            title: Text(comment),
+                          );
                         },
-                      ),
-                      SizedBox(
-                        height: 300,
-                        child: FutureBuilder<QuerySnapshot>(
-                          future: db
-                              .collection("movieComment")
-                              .where("movie_id", isEqualTo: widget.movieId)
-                              .get(),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return const Center(
-                                  child:
-                                      CircularProgressIndicator()); // Display a loading indicator while fetching data.
-                            }
-
-                            if (snapshot.hasError) {
-                              return Text('Error: ${snapshot.error}');
-                            }
-
-                            if (!snapshot.hasData ||
-                                snapshot.data!.docs.isEmpty) {
-                              return const Text('No comments available.');
-                            }
-
-                            // Replace 1 with the actual number of comments you want to display.
-                            int itemCount = snapshot.data!.size;
-
-                            return ListView.builder(
-                              itemCount: itemCount,
-                              itemBuilder: (context, index) {
-                                var data = snapshot.data!.docs[index];
-                                String comment = data["comment"];
-                                String user = data["user_email"];
-                                return ListTile(
-                                  contentPadding: const EdgeInsets.all(8),
-                                  leading: Text(user),
-                                  title: Text(comment),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      )
-                    ],
-                  )
-                : const Center(child: Text("이메일 인증하여 댓글을 달아보세요"))
-            : const Center(
-                child: Text("로그인하여 댓글을 달아보세요"),
-              ));
+                      );
+                    },
+                  ),
+                )
+              ],
+            ));
   }
 }
